3a199d880528fd3f91e66ffd65fdc3b17d69804f
Bhargav Krishna <bhargav.krishna@outlook.com>
2016-06-20 15:55:02 -0700

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+/// Gulp configuration for Typescript, SASS and Live Reload
+
+'use strict';
+
+// initialize all the requried libraries and files
+var gulp = require('gulp'),
+    browserSync = require('browser-sync').create(),
+    typescript = require('gulp-typescript'),
+    sourcemaps = require('gulp-sourcemaps'),
+    config = {
+        app: {
+            source: './src',
+            dest: './wwwroot'
+        },
+        browserSync: {
+            server: {
+                'baseDir': './wwwroot',
+                'routes': {
+                    '/node_modules': 'node_modules',
+                    '/bower_components': 'bower_components',
+                    '/rxjs': 'node_modules/rxjs'
+                }
+            }
+        }
+    };
+
+// create the typescript project for transpiling
+var tsProject = typescript.createProject('./tsconfig.json');
+
+// compile all typescript files
+gulp.task('compile:ts', function () {
+    var tsResult = tsProject.src()
+        .pipe(sourcemaps.init())
+        .pipe(typescript(tsProject))
+
+    tsResult.js
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(config.app.dest))
+        .pipe(browserSync.stream());
+});
+
+// copy anything that is not a sass file or typescript file
+gulp.task('copy', function () {
+    gulp.src([
+        config.app.source + '/**/*',
+        '!' + config.app.source + '/**/*.ts',
+    ], { base: config.app.source })
+        .pipe(gulp.dest(config.app.dest))
+});
+
+gulp.task('build', ['compile:sass', 'copy']);
+
+// start webserver and observe files for changes
+gulp.task('serve', ['build'], function () {
+    browserSync.init(config.browserSync);
+
+    gulp.watch(config.app.source + '/**/*.ts', ['compile:ts']);
+
+    gulp.watch([
+        config.app.source + '/**/*',
+        '!' + config.app.source + '/**/*.ts',
+    ], ['copy'])
+        .on('change', browserSync.reload);
+});
